ddf30817347361cf307de9620cb5e07b59583c8e
Charles <Charlessammarco@gmail.com>
2024-08-26 17:02:11 -0500

    set up pages

--- PokePopsicleJSX/src/App.jsx
+++ PokePopsicleJSX/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import PokePopsicle from './pages/PokePopsicle'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <PokePopsicle/>
     </>
   )
 }
---
+++ PokePopsicleJSX/src/pages/PokePopsicle.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+function PokePopsicle(){
+
+
+    return(
+        <h1>pokePop Page</h1>
+    )
+}
+
+export default PokePopsicle
