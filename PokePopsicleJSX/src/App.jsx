f6ab0c8dac6d64c6c5bf177f80d6099c6f95110f
Charles <Charlessammarco@gmail.com>
2024-09-05 13:28:47 -0500

    added popsicle pics and dropdown

--- PokePopsicleJSX/src/App.jsx
+++ PokePopsicleJSX/src/App.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import PokePopsicle from './pages/PokePopsicle'
+import { useState, useEffect } from "react";
+import "./App.css";
+import PokePopsicle from "./pages/PokePopsicle";
 
 function App() {
-  const [pokeData, setPokeData] = useState([])
+  const [pokeData, setPokeData] = useState([]);
 
   // useEffect(() =>{
   //   fetch("https://pokeapi.co/api/v2/pokemon/?limit=1025")
-    
+
   //     .then (r => r.json())
   //     .then(data => setPokeData(data.results))
   //     .catch(error => console.error('Error fetching data:', error))
   // },[])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://127.0.0.1:3001/results")
-    .then((r)=> r.json())
-    .then((items) => setPokeData(items))
-    .catch((error)=> (console.error("error", error)))
-  }, [])
+      .then((r) => r.json())
+      .then((items) => setPokeData(items))
+      .catch((error) => console.error("error", error));
+  }, []);
   // console.log(pokeData[0].name)
 
   return (
     <>
       <header>Pokémon Popsicle Creator</header>
-      <PokePopsicle pokeData={pokeData}/>
+      <PokePopsicle pokeData={pokeData} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- PokePopsicleJSX/src/pages/PokePopsicle.jsx
+++ PokePopsicleJSX/src/pages/PokePopsicle.jsx
@@ -1,46 +1,96 @@
-import { useState } from "react"
+import { useState } from "react";
+import popRed from "../assets/pop red.jpg";
+import popOrange from "../assets/pop orange.jpg";
+import popYellow from "../assets/pop yellow.jpg";
+import popGreen from "../assets/pop green.jpg";
+import popBlue from "../assets/pop blue.jpg";
+import popPurple from "../assets/pop purple.jpg";
+import popRainbow from "../assets/pop rainbow.jpg";
 
+function PokePopsicle({ pokeData }) {
+  const [selectedPokemon, setSelectedPokemon] = useState({});
+  const [pokemon, setPokemon] = useState(null);
+  const [selectPopsicle, setSelectPopsicle] = useState(null);
 
-function PokePopsicle({pokeData}){
+  const popsiclePics = [
+    { label: "Red", src: popRed },
+    { label: "Orange", src: popOrange },
+    { label: "Yellow", src: popYellow },
+    { label: "Green", src: popGreen },
+    { label: "Blue", src: popBlue },
+    { label: "Purple", src: popPurple },
+    { label: "Rainbow", src: popRainbow },
+  ];
 
-    const [ selectedPokemon, setSelectedPokemon] = useState({})
-    const [ pokemon, setPokemon] = useState(null)
+  function handleClick(e) {
+    console.log(e);
+    fetch(e)
+      .then((r) => {
+        if (!r.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return r.json();
+      })
+      .then((pokemon) => {
+        setPokemon(pokemon);
+      })
+      .catch((error) => {
+        console.error("Error fetching Pokémon data:", error);
+      });
+  }
 
-    function handleClick(e){
-        console.log(e)
-        fetch(e)
-        .then (r => {
-            if(!r.ok){
-                throw new Error("Network response was not ok");
-            }
-            return r.json()
-        })
-        .then ((pokemon) => {
-            setPokemon(pokemon)
-        })
-        .catch((error) => {
-            console.error("Error fetching Pokémon data:", error)
-        })
-    }
+  function handleSelectChange(e) {
+    const selectedPopsicle = e.target.value;
+    const selected = popsiclePics.find((p) => p.label === selectedPopsicle);
+    setSelectPopsicle(selected.src);
+  }
 
-   
-    return(
-        <>
+  return (
+    <>
+      <div>
         <h1>PokéPop Page</h1>
         <h2>Choose your Pokémon</h2>
-        <select onChange={(e)=>setSelectedPokemon(e.target.value)}>
-            <option>Select a Pokémon</option>
-                {pokeData.map((pokemon, index)=>(
-                    <option key = {pokemon.name} value = {pokemon.url}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.substring(1).toLowerCase()} -- #{
-                        index < 999 ? ("00" + parseInt(index + 1)).slice(-3) : parseInt(index + 1)
-                        }</option>
-                ))}
+        <select onChange={(e) => setSelectedPokemon(e.target.value)}>
+          <option>Select a Pokémon</option>
+          {pokeData.map((pokemon, index) => (
+            <option key={pokemon.name} value={pokemon.url}>
+              {pokemon.name.charAt(0).toUpperCase() +
+                pokemon.name.substring(1).toLowerCase()}{" "}
+              -- #
+              {index < 999
+                ? ("00" + parseInt(index + 1)).slice(-3)
+                : parseInt(index + 1)}
+            </option>
+          ))}
         </select>
-        <button onClick={()=> handleClick(selectedPokemon)}>Select</button>
+        <button onClick={() => handleClick(selectedPokemon)}>Select</button>
         <p></p>
-        {pokemon && <img src={pokemon.sprites.front_default} alt={pokemon.name}/> }
-        </>
-    )
+        {pokemon && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+      </div>
+
+      <div>
+        <h1>Popsicle Colors</h1>
+        <h2>Select a color of selectPopsicle</h2>
+
+        <select onChange={handleSelectChange}>
+          <option>select a color</option>
+          {popsiclePics.map((popsicle, index) => (
+            <option key={index} value={popsicle.label}>
+              {popsicle.label}
+            </option>
+          ))}
+        </select>
+
+        <button onClick={() => console.log(selectPopsicle)}>Select</button>
+        <p></p>
+        {selectPopsicle && (
+          <img src={selectPopsicle} style={{ width: "100px" }} />
+        )}
+      </div>
+    </>
+  );
 }
 
-export default PokePopsicle
+export default PokePopsicle;
